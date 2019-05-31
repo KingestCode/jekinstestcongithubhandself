039a373945b6cc070e0ab98f9920fb3828df54b3
张颖豪 <zhangyinghao@iMacdeMacBook-Pro.local>
2019-05-31 10:44:36 +0800

    first commit

---
+++ src/main/java/com/zyh/jekinstestcongithubhandself/JenkinsTestApplication.java
@@ -0,0 +1,11 @@
+package com.zyh.jekinstestcongithubhandself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+}
